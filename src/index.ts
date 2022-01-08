bad000f6975c488844adcf1c662e2e8ca71c9958
matmoimjs <matmoimjs@gmail.com>
2022-01-08 07:42:03 +0200

    add exports from index

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { WsChannelEnum } from './ws/ws-channel.enum';
+export { ApiChannelEnum } from './api/api-channel.enum';
+export { RootWsChannelEnum } from './root/root-ws-channel.enum';
+export { RootApiChannelEnum } from './root/root-api-channel.enum';
